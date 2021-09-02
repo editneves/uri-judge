684e9cb4481822b2199c50665682fa6d5617a0fa
Ruan Herculanno <ruannawe95@outlook.com>
2021-09-02 01:03:39 +0000

    creates a new console project for each new exercise

---
+++ 1001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            int a;
+            int b;
+
+            val = Console.ReadLine();
+            a = Convert.ToInt32(val);
+
+            val = Console.ReadLine();
+            b = Convert.ToInt32(val);
+
+            // display the line
+            Console.WriteLine("X = {0}", a+b);
+        }
+    }
+}
---
+++ 1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+        
+            string r = Console.ReadLine();
+
+            double raio = double.Parse(r);
+            double area = pi * Math.Pow(raio, 2);
+            Console.WriteLine("A={0:F4}", area);
+        }
+    }
+}
