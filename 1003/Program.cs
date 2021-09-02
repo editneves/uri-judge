430436a2227317ab8531ef08f415fecb1dea073e
Ruan Herculanno <ruannawe95@outlook.com>
2021-09-02 01:41:12 +0000

    resolvo programa 1003

---
+++ 1003/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A= int.Parse(Console.ReadLine());
+            int B= int.Parse(Console.ReadLine());
+            int soma = A + B;
+
+            Console.WriteLine($"SOMA = {soma}");
+            // Console.WriteLine("SOMA = {0}", soma);
+        }
+    }
+}
